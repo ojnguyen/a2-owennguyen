7b01a72268357056e6ad39ed18b203d0cc8379f1
Owen Nguyen <owennguyen18@gmail.com>
2026-09-04 03:57:13 -0400

    HTML and CSS

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const http = require('http'),
 
 // NOTE: Weight will be in pounds, reps will be an integer, and ORM will be a float rounded to 2 decimal places.
 let lifts = [
-  { ID: 1, exercise: 'bench press', weight: 185, reps: 6 },
-  { ID: 2, exercise: 'squat', weight: 285, reps: 8 },
-  { ID: 3, exercise: 'deadlift', weight: 315, reps: 6 }
+  { ID: 1, exercise: 'bench press', weight: 185, reps: 6, orm: 0.00 },
+  { ID: 2, exercise: 'squat', weight: 285, reps: 8, orm: 0.00 },
+  { ID: 3, exercise: 'deadlift', weight: 315, reps: 6, orm: 0.00 }
 ];
 let nextID = 4; // TODO: Assign this to 1 once this array is empty.
 
